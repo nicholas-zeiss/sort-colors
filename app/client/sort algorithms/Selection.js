c3687d8b5df5243b3f48a2229e6ff6b478786632
nicholas-zeiss <nicholas.zeiss@gmail.com>
2017-11-14 00:31:05 -0500

    refactored selection sort to use a generator

--- app/client/sort algorithms/Selection.js
+++ app/client/sort algorithms/Selection.js
@@ -1,65 +1,66 @@
 /**
-Here we have the class responsible for implementing selection sort
+ *
+ *	Here we have the class responsible for implementing selection sort
+ *
 **/
 
-import COLORS from '../utils/Colors';
+import Algorithm from './Algorithm';
+
+import { genColorMap, genColorRange, genColorSet } from '../utils/Colors';
 
 
-class Selection {
+class Selection extends Algorithm {
 	constructor(data) {
-		this.data = data;
+		super(data);
 
 		this.index = 0;
-		this.unsorted = 0;				  				//beginning of unsorted section
-		
-		this.min = [0, data[0]];						//minimum we find each traversal of the unsorted section
-		
-		this.inSwap = false;				        //used to split swaps across multiple ticks
-		
-		this.sorted = false;
+		this.inSwap = false;
+		this.minIndex = 0;
+		this.minValue = data[0];
+		this.unsorted = 0;	
 	}
 
-	//moves selection sort forward by one comparison or swap
-	//returns [array data, bool sorted, array colorScheme]
-	tick() {
-		let active = this.index;
 
-		if (this.sorted) {
-			return [this.data, true, [[0, this.data.length, COLORS.green]]];
+	* tick() {
+		while (this.unsorted != this.data.length) {
+			if (this.inSwap) {
+				this.swap(this.unsorted, this.minIndex);
+				this.inSwap = false;			
+				this.index = ++this.unsorted;
+				this.minIndex = this.index;
+				this.minValue = this.data[this.index];
 
-		} else if (this.inSwap) {
-			this.inSwap = false, active = this.unsorted;
-			
-			[this.data[this.unsorted], this.data[this.min[0]]] = [this.data[this.min[0]], this.data[this.unsorted]];
-			
-			this.index = ++this.unsorted;	
-			this.min = [this.index, this.data[this.index]];
+			} else if (this.index < this.data.length) {
+				if (this.minValue > this.data[this.index]) {
+					this.minIndex = this.index;
+					this.minValue = this.data[this.index];
+				}
+				
+				this.index++;
 
-		} else if (this.index < this.data.length) {
-			this.min = this.min[1] < this.data[this.index] ? this.min : [this.index, this.data[this.index]];
-			this.index++;
+			} else if (this.index == this.data.length) {
+				this.inSwap = true;
 
-		} else if (this.unsorted == this.data.length) {
-			this.sorted = true;
+			} else {
+				this.index++;
+			}
 
-		} else if (this.index == this.data.length) {
-			this.inSwap = true, active = this.min[0];
-
-		} else {
-			active = this.index++;
+			yield({ colors: this.genColors(), data: this.data });
 		}
 
-		//red for active, orange for smallest item found so far in unsorted section, green for sorted
-		return [
-			this.data,
-			this.sorted,
-			[
-				[active, active, COLORS.red],
-				[this.min[0], this.min[0], COLORS.orange],
-				[0, this.unsorted - 1, COLORS.green]
-			]
-		];
+		return this.finish();
+	}
+
+
+	genColors() {
+		return genColorMap(this.data.length, [
+			genColorRange(0, this.unsorted, 'green'),
+			genColorSet(new Set([ this.minIndex ]), 'orange'),
+			genColorSet(new Set([ this.index ]), 'red')
+		]);
 	}
 }
 
+
 export default Selection;
+
