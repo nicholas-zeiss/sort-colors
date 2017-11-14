29b0f8f3608b6409d963ba1937bde9340e35b0f7
nicholas-zeiss <nicholas.zeiss@gmail.com>
2017-11-13 21:23:18 -0500

    refactored Settings

--- app/client/components/Settings.js
+++ app/client/components/Settings.js
@@ -1,6 +1,8 @@
 /**
-This component allows us to alter the delay between ticks and the amount of items to sort. The state allows us to handle the user holding
-down a button to change values continuously rather than have to click repeatedly. 
+ *
+ *	This component allows us to alter the delay between ticks and the amount of items to sort. The state allows us to handle the user holding
+ *	down a button to change values continuously rather than have to click repeatedly. 
+ *
 **/
 
 import React from 'react';
@@ -10,79 +12,92 @@ class Settings extends React.Component {
 	constructor(props) {
 		super(props);
 
-		//while there should only be one interval running at a time, erratic clicking can create more, 
-		//hence we must keep track of multiple intervals in an array
-		this.state = {
-			intervals: [],
-			mouseStillDown: false
-		};
+		// while there should only be one interval running at a time, erratic clicking can create more, 
+		// hence we must keep track of multiple intervals in an array
+		this.state = { intervals: [], mouseStillDown: false };
 	}
 
-	//first perform action once to allow for clicks. 500ms after the mouse is clicked down our timeout checks whether the mouse
-	//is still down, and if it is, sets an interval to repeat the action and stores that interval id in the state.
-	repeat(action) {
+	// first perform action once to allow for clicks. 500ms after the mouse is clicked down our timeout checks whether the mouse
+	// is still down, and if it is, sets an interval to repeat the action and stores that interval id in the state.
+	repeat = action => {
 		action();
 
 		setTimeout(() => {
 			if (this.state.mouseStillDown) {
-				let id = setInterval(action, 100);
-				
-				this.setState({
-					intervals: this.state.intervals.concat(id)
-				});
+				const id = setInterval(action, 100);
+				this.setState({ intervals: [ ...this.state.intervals, id] });
 			}
 		}, 500);
 
-		this.setState({
-			mouseStillDown: true
-		});
+		this.setState({ mouseStillDown: true });
 	}
 
 
-	stopRepeat() {
+	stopRepeat = () => {
 		this.state.intervals.forEach(id => clearInterval(id));
-
-		this.setState({
-			intervals: [],
-			mouseStillDown: false
-		});
+		this.setState({ intervals: [], mouseStillDown: false });
 	}
 
 
 	render() {
+		const props = this.props;
+
+		const decDelay = () => this.repeat(props.changeDelay.bind(null, -5));
+		const incDelay = () => this.repeat(props.changeDelay.bind(null, 5));
+
+		const decDataSize = () => this.repeat(props.changeDataSize.bind(null, -5));
+		const incDataSize = () => this.repeat(props.changeDataSize.bind(null, 5));
+
 		return (
-			<div id='settings' className='btn-toolbar pull-right'>
+			<div className='btn-toolbar pull-right' id='settings'>
 				<div className='btn-group' id='settings-delay'>
-					<button onMouseDown={this.repeat.bind(this, this.props.changeDelay.bind(null, -5))}
-									onMouseUp={this.stopRepeat.bind(this)} type='button'
-									type='button'
-									className='btn btn-default'
-									disabled={this.props.sorting}>
+					<button 
+						className='btn btn-default'
+						disabled={ props.sorting } 
+						onMouseDown={ decDelay }
+						onMouseUp={ this.stopRepeat }
+						type='button'
+					>
 						<span className='glyphicon glyphicon-minus'></span>
 					</button>
-					<button className='btn btn-primary' disabled={this.props.sorting}>{'Delay (ms): ' + this.props.delay}</button>
-					<button onMouseDown={this.repeat.bind(this, this.props.changeDelay.bind(null, 5))}
-									onMouseUp={this.stopRepeat.bind(this)}
-									type='button'
-									className='btn btn-default'
-									disabled={this.props.sorting}>
+					
+					<button className='btn btn-primary' disabled={ props.sorting }>
+						{ 'Delay (ms): ' + props.delay }
+					</button>
+					
+					<button
+						className='btn btn-default'
+						disabled={ props.sorting }
+						onMouseDown={ incDelay }
+						onMouseUp={ this.stopRepeat }
+						type='button'
+					>
 						<span className='glyphicon glyphicon-plus'></span>
 					</button>
 				</div>
+				
 				<div className='btn-group' id='settings-items'>
-					<button onMouseDown={this.repeat.bind(this, this.props.changeDataSize.bind(null, -5))}
-									onMouseUp={this.stopRepeat.bind(this)}
-									type='button'
-									className='btn btn-default'
-									disabled={this.props.sorting}>
+					<button
+						className='btn btn-default'
+						disabled={ props.sorting }
+						onMouseDown={ decDataSize }
+						onMouseUp={ this.stopRepeat }
+						type='button'
+					>
 						<span className='glyphicon glyphicon-minus'></span>
 					</button>
-					<button className='btn btn-primary' disabled={this.props.sorting}>{'Items: ' + this.props.numItems}</button>
-					<button onMouseDown={this.repeat.bind(this, this.props.changeDataSize.bind(null, 5))}
-									onMouseUp={this.stopRepeat.bind(this)}
-									type='button'
-									className='btn btn-default'
-									disabled={this.props.sorting}>
+					
+					<button className='btn btn-primary' disabled={ props.sorting }>
+						{ 'Items: ' + props.numItems }
+					</button>
+					
+					<button
+						className='btn btn-default'
+						disabled={ props.sorting }
+						onMouseDown={ incDataSize }
+						onMouseUp={ this.stopRepeat }
+						type='button'
+					>
 						<span className='glyphicon glyphicon-plus'></span>
 					</button>
 				</div>
@@ -91,4 +106,6 @@ class Settings extends React.Component {
 	}
 }
 
+
 export default Settings;
+
